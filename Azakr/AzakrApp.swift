ec500a5fcc1cb756336d0bdbeb9f3a2870a2fe3c
hosam <hosamelmalt@gmail.com>
2021-03-25 10:20:11 +0200

    Initial Commit

---
+++ Azakr/AzakrApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzakrApp.swift
+//  Azakr
+//
+//  Created by hosam on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct AzakrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
